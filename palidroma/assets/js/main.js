962e2801974b09939608995f82c8020766ac28f6
odon-airoldi <odon.airoldi@gmail.com>
2026-02-09 15:50:42 +0100

    start of palindrome exercise

---
+++ palidroma/assets/js/main.js
@@ -0,0 +1,33 @@
+// const my_number_1 = 10;
+// const my_number_2 = 20;
+
+// function sum_number(number_1, number_2) {
+//     return number_1 + number_2
+// }
+// const sum_total = sum_number(my_number_1, my_number_2);
+// console.log(sum_total)
+
+
+
+
+
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+//                 123456
+const user_word = 'parola'
+
+const arr_user_word_reverse = [];
+
+
+for(i = user_word.length - 1; i >= 0; i-- ) {
+
+    array_user_word_reverse.push(user_word[i])
+
+    user_word_reverse = arr_user_word_reverse.join('');
+}
+
+console.log(arr_user_word_reverse)
+console.log(user_word_reverse)
+
