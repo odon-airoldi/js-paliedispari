b5850e3f5778ebaab8d918ccef42bc6418cc3d42
odon-airoldi <odon.airoldi@gmail.com>
2026-02-09 15:19:34 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const my_number_1 = 10;
+const my_number_2 = 20;
+
+
+
+function sum_number(number_1, number_2) {
+    return number_1 + number_2
+}
+const sum_total = sum_number(my_number_1, my_number_2);
+console.log(sum_total)
+
+
+
+function div_number(number_1, number_2) {
+    return number_1 / number_2
+}
+const div_total = div_number(my_number_1, my_number_2);
+console.log(div_total)
+
+
+
+function mult_number(number_1, number_2) {
+    return number_1 * number_2
+}
+const mult_total = mult_number(my_number_1, my_number_2);
+console.log(mult_total)
